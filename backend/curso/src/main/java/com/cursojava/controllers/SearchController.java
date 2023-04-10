3fec48014b6f5d4d61779f5e6eb54197c76a9876
JohnMejia <john.mejias@udea.edu.co>
2023-04-10 17:54:43 -0500

    se agrega cross-origin al controller del backend

--- backend/curso/src/main/java/com/cursojava/controllers/SearchController.java
+++ backend/curso/src/main/java/com/cursojava/controllers/SearchController.java
@@ -3,6 +3,7 @@ package com.cursojava.controllers;
 import com.cursojava.entities.WebPage;
 import com.cursojava.services.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin("*")
 public class SearchController {
 
     @Autowired
